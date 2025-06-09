19a72397c8fca061a6191af75f6abb72fb840241
James <j606888@gmail.com>
2025-06-09 11:47:33 +0800

    Setup tailwind and font-family

---
+++ app/routes/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <>
+      <div className="font-noto text-2xl ">這是About (Noto Sans TC)</div>
+      <div className="font-roboto text-2xl">這是About (Roboto)</div>
+    </>
+  )
+}
