45c16c3ee27f3a7559d06c4138c8916f43a6e7f0
James <j606888@gmail.com>
2025-06-09 18:07:22 +0800

    Update

--- app/containers/Home/Hero.jsx
+++ app/containers/Home/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="bg-[url('./home-header-sm.png')] xl:bg-[url('./home-header.png')] h-[396px] bg-cover bg-center xl:h-[787px] ">
+    <section className="bg-[url('/home-header-sm.png')] xl:bg-[url('/home-header.png')] h-[396px] bg-cover bg-center xl:h-[787px] ">
       <div className="flex flex-col items-end justify-center mx-auto max-w-7xl h-full px-[14px] md:px-9">
         <p className="text-base text-[#650300] md:text-4xl">Promise-Desert 2020 早春系列</p>
         <h2 className="text-[#650300] text-2xl mb-2 font-bold md:text-[64px] md:mb-1 ">
